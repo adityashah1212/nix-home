6e97ece268f3af65f9c0846c668263694ed389a3
Aditya Shah <25403870+adityashah1212@users.noreply.github.com>
2026-01-09 11:33:25 -0800

    Remove wild linker as it doesn't seem to work

--- home/home.nix
+++ home/home.nix
@@ -40,7 +40,6 @@
     shellcheck
     nufmt
     skim
-    wild
   ];
 
   home.activation.starship-nu = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
@@ -55,21 +54,6 @@
     text = ''
       $env.CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
       source $"($nu.cache-dir | path join carapace.nu)"
-    '';
-  };
-
-  home.file.".cargo/config.toml" = {
-    text = ''
-      [unstable]
-      codegen-backend = true
-
-      [profile.dev]
-      codegen-backend = "cranelift"
-
-      [target.'cfg(all(target_os = "linux", debug-assertions))']
-      rustflags = [
-        "-C", "link-arg=-fuse-ld=wild"
-      ]
     '';
   };
 
